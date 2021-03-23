b8b2a0fd83682552e7647d367f288d3ee3fbba45
Goun Lee <goun.lee@lge.com>
2021-03-23 17:48:38 +0900

    Added yml file to update copyright year automatically

--- test/testCFile.js
+++ test/testCFile.js
@@ -1,7 +1,7 @@
 /*
  * testCFile.js - test the c file handler object.
  *
- * Copyright © 2019-2021, JEDLSoft
+ * Copyright (c) 2019-2021, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- test/testSuiteFiles.js
+++ test/testSuiteFiles.js
@@ -1,7 +1,7 @@
 /*
  * testSuiteFiles.js - list the test files in this directory
  * 
- * Copyright © 2019-2021, JEDLSoft
+ * Copyright (c) 2019-2021, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
